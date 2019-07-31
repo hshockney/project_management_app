92af572a77fd43cf726d9dbd36957eaefaee7de8
hshockney <shockney@atlanticbb.net>
2019-07-31 10:16:40 -0400

    Team and project models and connections are set up

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   has_many :projects
   has_many :teams
-
+ 
   # new function to set the password without knowing the current
   # password used in our confirmation controller.
   def attempt_set_password(params)
